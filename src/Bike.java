906ffb63831b2e450be6e979de653f9524313969
WYYang1125 <3319239914@qq.com>
2026-03-28 21:28:57 +0800

    Refactor Bike class constructor and add ActiveRental

--- src/Bike.java
+++ src/Bike.java
@@ -1,44 +1,26 @@
-import java.time.LocalDateTime;
 
+import java.time.LocalDateTime;
 public class Bike {
     private String bikeID;
     private boolean isAvailable;
     private int batteryLevel;
     private LocalDateTime lastUsedTime;
     private String location;
-
-    public Bike(String bikeID, boolean isAvailable, int batteryLevel, LocalDateTime lastUsedTime, String location) {
-        this.bikeID = bikeID;
-        this.isAvailable = isAvailable;
-        this.batteryLevel = batteryLevel;
-        this.lastUsedTime = lastUsedTime;
-        this.location = location;
+    public Bike(String id, boolean avail, int bat, LocalDateTime time, String loc) {
+        bikeID=id; isAvailable=avail; batteryLevel=bat; lastUsedTime=time; location=loc;
     }
+    public String getBikeID(){return bikeID;}
+    public boolean isAvailable(){return isAvailable;}
+    public void setAvailable(boolean b){isAvailable=b;}
+    public void setLastUsedTime(LocalDateTime t){lastUsedTime=t;}
+    public String getLocation(){return location;}
+}
 
-    
-    public String getBikeID() { return bikeID; }
-    public void setBikeID(String bikeID) { this.bikeID = bikeID; }
-
-    public boolean isAvailable() { return isAvailable; }
-    public void setAvailable(boolean available) { isAvailable = available; }
-
-    public int getBatteryLevel() { return batteryLevel; }
-    public void setBatteryLevel(int batteryLevel) { this.batteryLevel = batteryLevel; }
-
-    public LocalDateTime getLastUsedTime() { return lastUsedTime; }
-    public void setLastUsedTime(LocalDateTime lastUsedTime) { this.lastUsedTime = lastUsedTime; }
-
-    public String getLocation() { return location; }
-    public void setLocation(String location) { this.location = location; }
-
-    @Override
-    public String toString() {
-        return "Bike{" +
-                "bikeID='" + bikeID + '\'' +
-                ", isAvailable=" + isAvailable +
-                ", batteryLevel=" + batteryLevel +
-                ", lastUsedTime=" + lastUsedTime +
-                ", location='" + location + '\'' +
-                '}';
+public class ActiveRental {
+    private String bikeID, userEmail;
+    private LocalDateTime tripStartTime;
+    public ActiveRental(String id, String email, LocalDateTime t) {
+        bikeID=id; userEmail=email; tripStartTime=t;
     }
+    public String getBikeID(){return bikeID;}
 }
