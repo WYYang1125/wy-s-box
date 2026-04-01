100bd39a761c2ad4bf61404da5ef25935b5c8bce
WYYang1125 <3319239914@qq.com>
2026-04-01 17:26:22 +0800

    Create BikeService.java

---
+++ src/BikeService.java
@@ -0,0 +1,12 @@
+public class BikeService{
+    public boolean findAvailableBikes(){
+        return true;
+    }
+    public boolean validateLocation(){
+        return true;
+    }
+    public void reserveBike(){
+    }
+    public void releaseBike(){
+    }
+}
