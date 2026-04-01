a64d7e97a69d3cb9639547a66f29f5de7cebe271
WYYang1125 <3319239914@qq.com>
2026-04-01 17:26:41 +0800

    Create RentalService.java

---
+++ src/RentalService.java
@@ -0,0 +1,10 @@
+public class RentalService{
+    public void startRental(){
+    }
+    public void endRental(){
+    }
+    public void cancelRental(){
+    }
+    public void trackActiveRentals(){
+    }
+}
